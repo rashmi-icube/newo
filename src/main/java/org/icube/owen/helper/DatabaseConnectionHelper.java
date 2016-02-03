25e5513ce8c0c37c460fdfd639c669242a1bccb5
swarna-icube <ssrivastava@i-cube.in>
2016-02-03 10:08:57 +0530

    db helper cleanup

--- src/main/java/org/icube/owen/helper/DatabaseConnectionHelper.java
+++ src/main/java/org/icube/owen/helper/DatabaseConnectionHelper.java
@@ -14,30 +14,6 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class DatabaseConnectionHelper {
 
-	/*
-	 * public static void main(String args[]) { try {
-	 * Class.forName("com.mysql.jdbc.Driver"); Connection conn =
-	 * DriverManager.getConnection( "jdbc:mysql://localhost:3306/owen", "root",
-	 * "root");
-	 * 
-	 * Statement stmt = conn.createStatement();
-	 * 
-	 * ResultSet rs = stmt.executeQuery("select * from dimension_master");
-	 * Map<Integer, String> filterLabelMap = new HashMap<>(); while (rs.next())
-	 * { System.out.println(rs.getInt(1) + "  " + rs.getString(2));
-	 * filterLabelMap.put(rs.getInt(1),rs.getString(2)); } Statement stmt1 =
-	 * conn.createStatement(); ResultSet rs1 = stmt.executeQuery(
-	 * "select * from initiative_type where Category = 'Individual'");
-	 * Map<Integer, String> initiativeTypeMap = new HashMap<>();
-	 * while(rs1.next()){ System.out.println(rs1.getInt(1) + "  " +
-	 * rs1.getString(2)); initiativeTypeMap.put(rs1.getInt(1),rs1.getString(2));
-	 * }
-	 * 
-	 * conn.close();
-	 * 
-	 * } catch (Exception e) { System.out.println(e); } }
-	 */
-
 	private final static String DB_PATH = getDatabaseConnectionDetails();
 	// private final static String DB_PATH =
 	// "C:\\Users\\fermion10\\Documents\\Neo4j\\graph.db";
@@ -59,23 +35,19 @@ public class DatabaseConnectionHelper {
 			Properties props = new Properties();
 			props.load(reader);
 			dbPath = props.getProperty("db_path");
-			org.apache.log4j.Logger.getLogger(DatabaseConnectionHelper.class)
-					.debug("DB Path : " + dbPath);
+			org.apache.log4j.Logger.getLogger(DatabaseConnectionHelper.class).debug("DB Path : " + dbPath);
 			reader.close();
 		} catch (FileNotFoundException ex) {
-			org.apache.log4j.Logger.getLogger(DatabaseConnectionHelper.class)
-					.error("Database path file doesn't exist", ex);
+			org.apache.log4j.Logger.getLogger(DatabaseConnectionHelper.class).error("Database path file doesn't exist", ex);
 		} catch (IOException ex) {
-			org.apache.log4j.Logger.getLogger(DatabaseConnectionHelper.class)
-					.error("IOException in Database", ex);
+			org.apache.log4j.Logger.getLogger(DatabaseConnectionHelper.class).error("IOException in Database", ex);
 		}
 		return dbPath;
 	}
 
 	public DatabaseConnectionHelper() {
 
-		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File(
-				DB_PATH));
+		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File(DB_PATH));
 
 		try {
 
@@ -83,30 +55,21 @@ public class DatabaseConnectionHelper {
 
 			mysqlCon = DriverManager.getConnection(mysqlurl, user, password);
 
-			System.out
-					.println("Successfully connected to MySql with owen database");
+			System.out.println("Successfully connected to MySql with owen database");
 		} catch (SQLException e) {
-			System.out
-					.println("An error occurred. Maybe user/password is invalid");
+			System.out.println("An error occurred. Maybe user/password is invalid");
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		registerShutdownHook(graphDb);
-		/*
-		 * try { if (!mysqlCon.isClosed()) { try { mysqlCon.close();
-		 * System.out.println("Connection to MySql closed!!!!"); } catch
-		 * (SQLException e) { e.printStackTrace(); } } } catch (SQLException e)
-		 * { e.printStackTrace(); }
-		 */
+
 	}
 
 	public void shutDown() {
 		System.out.println();
 		System.out.println("Shutting down database ...");
-		org.apache.log4j.Logger.getLogger(DatabaseConnectionHelper.class)
-				.debug("Shutting down database ...");
+		org.apache.log4j.Logger.getLogger(DatabaseConnectionHelper.class).debug("Shutting down database ...");
 
 		graphDb.shutdown();
 		try {
@@ -125,11 +88,10 @@ public class DatabaseConnectionHelper {
 	}
 
 	/**
-	 * Registers a shutdown hook for the Neo4j instance so that it shuts down
+	 * Registers a shutdown hook for the Neo4j instance so that it shuts down 
 	 * nicely when the VM exits (even if you "Ctrl-C" the running application).
 	 * 
-	 * @param graphDb
-	 *            - A database factory object
+	 * @param graphDb - A database factory object
 	 */
 	public void registerShutdownHook(final GraphDatabaseService graphDb) {
 
