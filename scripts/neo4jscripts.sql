19e41077d27b7e6ce9a17fafe0654d05176bf6a2
hpatelicube <hpatel@i-cube.in>
2016-02-08 12:33:09 +0530

    added procedure for configure page

--- scripts/neo4jscripts.sql
+++ scripts/neo4jscripts.sql
@@ -1,5 +1,9 @@
 /*smart list for individual*/
+
+
 /* emplistcomes from sql op*/
+call getListCollegue(emplist)
+
 
 match (a:Employee)<-[r:relaition]-(b:Employee) where a.emp_id in emplist and b.emp_id in emplist
 return a.emp_id as employeeId, a.FirstName as firstName, a.LastName as lastName,
