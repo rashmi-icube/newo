0c5de430828f72f82ba6c9efaea21c5ecc160a30
swarna-icube <ssrivastava@i-cube.in>
2016-02-09 09:39:39 +0530

    BatchList class added

---
+++ src/main/java/org/icube/owen/configure/BatchList.java
@@ -0,0 +1,17 @@
+package org.icube.owen.configure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.icube.owen.ObjectFactory;
+import org.icube.owen.TheBorg;
+import org.icube.owen.helper.DatabaseConnectionHelper;
+
+public class BatchList extends TheBorg {
+
+	public List<Batch> getBatchList(){
+		List<Batch> batchList = new ArrayList<Batch>();
+		return batchList ;
+		
+	}
+}
