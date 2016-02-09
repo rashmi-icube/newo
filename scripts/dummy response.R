20f49472f5bfc2c816e9b6d17668f684acfbc2f0
hpatelicube <hpatel@i-cube.in>
2016-02-09 16:41:18 +0530

    updated script

--- scripts/dummy response.R
+++ scripts/dummy response.R
@@ -1,8 +1,11 @@
 setwd("C:\\Users\\Hitendra\\Desktop\\sqldb\\survey response")
 question=read.csv("question.csv",stringsAsFactors = FALSE)
 
-question$startdate=strptime(question$startdate,"%Y-%m-%d")
-question$enddate=strptime(question$enddate,"%Y-%m-%d")
+question$startdate=as.Date(question$startdate,"%Y-%m-%d")
+question$enddate=as.Date(question$enddate,"%Y-%m-%d")
+
+today <- Sys.Date()
+question=question[question$startdate<=today,]
 
 employee=read.csv("employee.csv")
 me_response=data.frame(emp_id=as.numeric(),que_id=as.numeric(),response_time=as.character(),semtiment_weight=as.character(),rel_id=as.numeric())
@@ -27,18 +30,18 @@ for (i in 1:nrow(question)){
       me_response=rbind(me_response,data.frame(emp_id,que_id,response_time,sentiment_weight,rel_id))
     }else{
       wesub=we_response[we_response$emp_id==emp_id & we_response$rel_id==rel_id,]
-      currcube=employee$cube_id[employee$emp_id==currresp]  
+      currcube=employee$cube_id[employee$emp_id==emp_id]  
       currcubeemp=employee$emp_id[employee$cube_id==currcube]
       currcubeempnot=employee$emp_id[!(employee$emp_id %in% currcubeemp)]
       if(nrow(wesub)==0){
-        target_emp_id_cube=currcubeemp[sample(1:length(currcubeemp),sample(4:7,1),replace = FALSE)]
-        target_emp_id_cubenot=currcubeempnot[sample(1:length(currcubeempnot),sample(1:3,1),replace = FALSE)]
+        target_emp_id_cube=currcubeemp[sample(1:length(currcubeemp),min(sample(4:7,1),length(currcubeemp)),replace = FALSE)]
+        target_emp_id_cubenot=currcubeempnot[sample(1:length(currcubeempnot),min(sample(1:3,1),length(currcubeempnot)),replace = FALSE)]
         target_emp_id=append(target_emp_id_cube,target_emp_id_cubenot)
       }else{
         target_emp_id_current=wesub$target_emp_id
         target_emp_id_same=target_emp_id_current[sample(1:length(target_emp_id_current),round(length(target_emp_id_current)*0.8,0),replace = FALSE)]
-        target_emp_id_cube=currcubeemp[sample(1:length(currcubeemp),sample(1:2,1),replace = FALSE)]
-        target_emp_id_cubenot=currcubeempnot[sample(1:length(currcubeempnot),sample(1:2,1),replace = FALSE)]
+        target_emp_id_cube=currcubeemp[sample(1:length(currcubeemp),min(sample(1:2,1),length(currcubeemp)),replace = FALSE)]
+        target_emp_id_cubenot=currcubeempnot[sample(1:length(currcubeempnot),min(sample(1:2,1),length(currcubeempnot)),replace = FALSE)]
         target_emp_id=append(target_emp_id_same,target_emp_id_cube,target_emp_id_cubenot)
         target_emp_id=unique(target_emp_id)
         #print(paste("done for question ",i))
