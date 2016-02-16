f9fa53bfa87a44010a397c6c3cb1833dd1c02427
hpatelicube <hpatel@i-cube.in>
2016-02-16 12:02:08 +0530

    added config script for r and new metric

---
+++ scripts/config.R
@@ -0,0 +1,16 @@
+
+mysqlusername='hpatel'
+
+mysqlpasswod="hitesh16"
+
+mysqldbname='owen'
+
+mysqlhost="192.168.1.13"
+
+mysqlport=3306
+
+neopath="http://192.168.1.13:7474/db/data/"
+
+neousername="neo4j"
+
+neopassword="hitesh16"
