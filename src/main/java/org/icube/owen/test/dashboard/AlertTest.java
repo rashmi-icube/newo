088615cc81da9cfb52ddc61b3decb410817cc3d3
rashmi-icube <rashmi@i-cube.in>
2016-02-29 10:14:44 +0530

    Split alertMessage to alertTeam+alertStatement

--- src/main/java/org/icube/owen/test/dashboard/AlertTest.java
+++ src/main/java/org/icube/owen/test/dashboard/AlertTest.java
@@ -8,20 +8,21 @@ import org.junit.Test;
 
 public class AlertTest {
 	Alert a = (Alert) ObjectFactory.getInstance("org.icube.owen.dashboard.Alert");
-	
+
 	@Test
-	public void testGet(){
+	public void testGet() {
 		Alert alert = a.get(4);
 		assertTrue(alert.getAlertId() > 0);
 		assertTrue(!alert.getEmployeeList().isEmpty());
-		assertTrue(!alert.getAlertMessage().isEmpty());
+		assertTrue(!alert.getAlertTeam().isEmpty());
+		assertTrue(!alert.getAlertStatement().isEmpty());
 		assertTrue(!alert.getAlertMetric().toString().isEmpty());
 		assertTrue(!alert.getFilterList().isEmpty());
-		
+
 	}
-	
+
 	@Test
-	public void testDelete(){
+	public void testDelete() {
 		boolean status = a.delete(2);
 		assertTrue(status == true);
 	}
