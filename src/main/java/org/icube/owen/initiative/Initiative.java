a5a4bf2589e1af7224898072513aa47edfcf2f89
rashmi-icube <rashmi@i-cube.in>
2016-03-11 11:15:29 +0530

    Store initiative metrics at the time of initiative creation

--- src/main/java/org/icube/owen/initiative/Initiative.java
+++ src/main/java/org/icube/owen/initiative/Initiative.java
@@ -3,6 +3,7 @@ package org.icube.owen.initiative;
 import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,7 +16,9 @@ import org.icube.owen.TheBorg;
 import org.icube.owen.employee.Employee;
 import org.icube.owen.filter.Filter;
 import org.icube.owen.helper.DatabaseConnectionHelper;
+import org.icube.owen.helper.UtilHelper;
 import org.icube.owen.metrics.Metrics;
+import org.icube.owen.metrics.MetricsList;
 
 public class Initiative extends TheBorg {
 
@@ -71,6 +74,7 @@ public class Initiative extends TheBorg {
 	public int create() {
 		DatabaseConnectionHelper dch = ObjectFactory.getDBHelper();
 		int initiativeId = 0;
+		int teamSize = 0;
 		try {
 			org.apache.log4j.Logger.getLogger(Initiative.class).debug("Creating the initiative");
 
@@ -111,34 +115,52 @@ public class Initiative extends TheBorg {
 					List<String> posParam = new ArrayList<>();
 
 					for (Filter f : this.filterList) {
-						if (f.getFilterName().equalsIgnoreCase("Function")) {
+						if (f.getFilterName().equalsIgnoreCase("Function") && !f.getFilterValues().keySet().contains(0)) {
 							funcParam.addAll(f.getFilterValues().values());
-						} else if (f.getFilterName().equalsIgnoreCase("Position")) {
+						} else if (f.getFilterName().equalsIgnoreCase("Position") && !f.getFilterValues().keySet().contains(0)) {
 							posParam.addAll(f.getFilterValues().values());
-						} else if (f.getFilterName().equalsIgnoreCase("Zone")) {
+						} else if (f.getFilterName().equalsIgnoreCase("Zone") && !f.getFilterValues().keySet().contains(0)) {
 							zoneParam.addAll(f.getFilterValues().values());
 						}
 					}
 
-					if (!funcParam.contains(0)) {
-						funcQuery = "f.Id in " + funcParam.toString();
-					}
-					if (!zoneParam.contains(0)) {
-						zoneQuery = "z.Id in " + zoneParam.toString();
-					}
+					funcQuery = funcParam.isEmpty() ? "" : "f.Id in " + funcParam.toString();
+					zoneQuery = zoneParam.isEmpty() ? "" : "z.Id in " + zoneParam.toString();
+					posQuery = posParam.isEmpty() ? "" : "p.Id in " + posParam.toString();
 
-					if (!posParam.contains(0)) {
-						posQuery = "p.Id in " + posParam.toString();
-					}
 					String query = "match (a:Employee)-[:has_functionality]->(f:Function), (p:Position)<-[:is_positioned]-(a)-[:from_zone]->(z:Zone) "
 							+ ((!zoneQuery.isEmpty() || !funcQuery.isEmpty() || !posQuery.isEmpty()) ? " where " : "")
 							+ (zoneQuery.isEmpty() ? "" : (zoneQuery + ((!funcQuery.isEmpty() || !posQuery.isEmpty() ? " and " : ""))))
 							+ (funcQuery.isEmpty() ? "" : funcQuery + (!posQuery.isEmpty() ? " and " : ""))
 							+ (posQuery.isEmpty() ? "" : (posQuery))
 							+ "  return count(a) as TeamSize";
-					
+
 					res = dch.neo4jCon.createStatement().executeQuery(query);
-					while (res.next()) {}
+
+					while (res.next()) {
+						teamSize = res.getInt("TeamSize");
+					}
+
+					MetricsList ml = new MetricsList();
+					List<Metrics> metricsList = ml.getInitiativeMetricsForTeam(initiativeId, this.filterList);
+					for (Metrics m : metricsList) {
+						CallableStatement cstmt = dch.mysqlCon.prepareCall("{call insertInitiativeMetricValue(?,?,?,?,?)}");
+						cstmt.setInt("initiativeid", initiativeId);
+						cstmt.setInt("metricid", m.getId());
+						cstmt.setInt("metricvalue", m.getScore());
+						cstmt.setTimestamp("calctime",Timestamp.from(Instant.now()));
+						cstmt.setInt("noemp", teamSize);
+						ResultSet rs = cstmt.executeQuery();
+						while (rs.next()) {
+							if (rs.getBoolean("op")) {
+								org.apache.log4j.Logger.getLogger(Initiative.class).debug(
+										"Success in storing the metrics initiative with metricId : " + m.getId());
+							} else {
+								org.apache.log4j.Logger.getLogger(Initiative.class).error(
+										"Unsuccessful in storing the metrics initiative with metricId : " + m.getId());
+							}
+						}
+					}
 
 				}
 				if (!this.ownerOfList.isEmpty() && setOwner(initiativeId, this.ownerOfList)) {
@@ -146,10 +168,6 @@ public class Initiative extends TheBorg {
 				} else {
 					org.apache.log4j.Logger.getLogger(Initiative.class).error("Unsuccessful in setting owner for initiative");
 				}
-				
-				 
-				
-				
 
 			} else {
 				org.apache.log4j.Logger.getLogger(Initiative.class).error("Unable to create initiative");
