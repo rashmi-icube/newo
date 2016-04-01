ea146eaf1685c40abb774eecdc8f561b02f13f6f
hpatelicube <hpatel@i-cube.in>
2016-04-01 10:03:08 +0530

    updated new logic for update neo

--- scripts/Job.R
+++ scripts/Job.R
@@ -5,8 +5,9 @@ library(igraph)
 library(moments)
 library(reshape2)
 
-#setwd("C:\\Users\\Hitendra\\Desktop\\R metric Function")
 #source('config.R')
+
+setwd("C:\\Users\\Hitendra\\Desktop\\comp1\\neo")
 
 mysqlusername='hpatel'
 
@@ -20,7 +21,7 @@ mysqlhost="localhost"
 mysqlport=3306
 
 
-calcualte_edge=function(CompanyId){
+calculate_edge=function(CompanyId){
   mydb = dbConnect(MySQL(), user=mysqlusername, password=mysqlpasswod, dbname=mysqldbname, host=mysqlhost, port=mysqlport)
   
   query=paste("call getCompanyConfig(",CompanyId,");",sep = "")
@@ -119,6 +120,10 @@ calcualte_edge=function(CompanyId){
   
   
   TableName=paste("calculated_edge_",format(Sys.Date(), "%Y_%m_%d"),sep = "")
+  
+  query=paste("drop table if exists ",TableName,";")
+  
+  dbGetQuery(mydb,query)
   
   query=paste("CREATE TABLE ",TableName,"(
               `emp_from` int(11) NOT NULL,
@@ -298,6 +303,73 @@ calcualte_edge_old=function(CompanyId){
 } 
 
 update_neo=function(CompanyId){
+  
+  mydb = dbConnect(MySQL(), user=mysqlusername, password=mysqlpasswod, dbname=mysqldbname, host=mysqlhost, port=mysqlport)
+  
+  query=paste("call getCompanyConfig(",CompanyId,");",sep = "")
+  res <- dbSendQuery(mydb,query)
+  CompanyConfig=fetch(res,-1)
+  
+  dbDisconnect(mydb)
+  
+  comp_sql_dbname=CompanyConfig$comp_sql_dbname[1]
+  comp_sql_server=CompanyConfig$sql_server[1]
+  comp_sql_user_id=CompanyConfig$sql_user_id[1]
+  comp_sql_password=CompanyConfig$sql_password[1]
+  
+  mydb = dbConnect(MySQL(), user=comp_sql_user_id, password=comp_sql_password, dbname=comp_sql_dbname, host=comp_sql_server, port=mysqlport)
+  
+  
+  TableName=paste("calculated_edge_",format(Sys.Date(), "%Y_%m_%d"),sep = "")
+  
+  query=paste("SELECT * FROM ",TableName," ;",sep = "")
+  res <- dbSendQuery(mydb,query)
+  edgelist=fetch(res,-1)
+  
+  query="SELECT * FROM relationship_master"
+  res <- dbSendQuery(mydb,query)
+  relationship_master=fetch(res,-1)
+  
+  edgelist=merge(edgelist,relationship_master,by="rel_id")
+  
+  dbDisconnect(mydb)
+  
+  com_neopath=CompanyConfig$neo_db_url[1]
+  com_neopath=paste(com_neopath,"/db/data/",sep = "")
+  
+  com_neousername=CompanyConfig$neo_user_name[1]
+  com_neopassword=CompanyConfig$neo_passsword[1]
+  
+  graph = startGraph(com_neopath, username=com_neousername, password=com_neopassword)
+  
+  
+  querynode = "match (a:Employee)-[r:innovation|learning|mentor|social]->(b:Employee) delete r"
+  
+  cypher(graph, querynode) 
+  
+  for (k in 1:4){
+    edgelistrel=edgelist[edgelist$rel_id==k,]
+    
+    relname=relationship_master$rel_name[relationship_master$rel_id==k]
+    
+    write.csv(edgelist,"edgelist.csv",row.names = FALSE)
+    
+    link=getwd()
+    
+    query=paste('LOAD CSV WITH HEADERS FROM "file:///',link,'/edgelist.csv" AS row
+                MATCH (a:Employee),(b:Employee)
+                WHERE a.emp_id = toInt(row.emp_from) AND b.emp_id = toInt(row.emp_to)
+                CREATE (a)-[r:',relname,' {weight:toInt(row.weight)}]->(b)',sep = "")
+    
+    cypher(graph, query) 
+    
+  }
+  
+  return(TRUE)
+}
+
+
+update_neo_old=function(CompanyId){
   
   mydb = dbConnect(MySQL(), user=mysqlusername, password=mysqlpasswod, dbname=mysqldbname, host=mysqlhost, port=mysqlport)
   
