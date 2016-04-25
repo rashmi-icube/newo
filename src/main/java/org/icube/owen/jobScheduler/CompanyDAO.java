9635950e20d9e398212fd96495122c5b951e313d
rashmi-icube <rashmi@i-cube.in>
2016-04-25 09:25:26 +0530

    Inline comments

--- src/main/java/org/icube/owen/jobScheduler/CompanyDAO.java
+++ src/main/java/org/icube/owen/jobScheduler/CompanyDAO.java
@@ -2,7 +2,6 @@ package org.icube.owen.jobScheduler;
 
 import java.sql.Date;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.TimerTask;
